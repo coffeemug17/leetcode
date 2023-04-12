3c40a240b0d5657bf118c4172bf077327d7e0c9d
Mitul Pandey <mitulpandey2@gmail.com>
2023-04-12 14:21:32 +0100

    Time: 50 ms (98.93%), Space: 15.1 MB (36.41%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashmap = {}
-        for idx, num in enumerate(nums):
+        
+        for i, num in enumerate(nums):
             diff = target - num
             if diff in hashmap:
-                return [hashmap[diff], idx]
-            hashmap[num] = idx
-            
+                return [hashmap[diff], i]
+            hashmap[num] = i
