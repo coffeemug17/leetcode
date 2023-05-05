69f846361ee909bc41872720f20c805096f204d1
Mitul Pandey <mitulpandey2@gmail.com>
2023-05-06 00:27:08 +0100

    Time: 141 ms (9.08%), Space: 22.2 MB (5.07%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,9 +2,8 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
         res = defaultdict(list)
-        
         for str in strs:
-            count = [0] * 26 #to account for all the characters a - z
+            count = [0] * 26 # a to z
             for char in str:
                 count[ord(char) - ord('a')] += 1
             res[tuple(count)].append(str)
